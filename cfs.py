f43929cea507090950a8ce3be1cda0239c5f5b52
Pubudu-Anuradha <pubudu.anuradha.satharasinghe@gmail.com>
2021-11-13 22:39:57 +0530

    changed runners to work with a json

--- cfs.py
+++ cfs.py
@@ -5,17 +5,17 @@ from sys import argv
 from os import chdir, listdir, mkdir, getcwd, system
 from bs4 import BeautifulSoup as Bs
 from requests import get
-
+from json import loads
 # checking if the link was passed as a command line argument
 # asking for an input otherwise
-if len(argv)>1:
+if len(argv) > 1:
     htm = get(argv[1])
 else:
     htm = get(input("Enter the problem link : "))
 soup = Bs(htm.text, 'html.parser')
 
 # getting problem name
-Name = str(soup.find('div', {'class': 'title'}).string).replace(' ','_')
+Name = str(soup.find('div', {'class': 'title'}).string).replace(' ', '_')
 
 # saving current working directory for later
 wd = getcwd()
@@ -32,16 +32,17 @@ chdir(templatePath)
 
 # processing the runners file
 # runners is a dictionary with the script contents and the command mapped to the file extension
-runners = {}
-runnersFile = open("cfs.txt", 'r')
-line = runnersFile.readline()
-while line:
-    line = line.strip()
-    if not (line.startswith('#') or len(line) < 2):
-        ext, comp, run,vim = line.split(':')
-        runners[ext.strip()] = {'compile': comp.strip().replace(
-            'NAME', Name), 'run': run.strip().replace('NAME', Name),'vim' : vim.strip()}
-    line = runnersFile.readline()
+# runners = {}
+# runnersFile = open("cfs.txt", 'r')
+# line = runnersFile.readline()
+# while line:
+#     line = line.strip()
+#     if not (line.startswith('#') or len(line) < 2):
+#         ext, comp, run,vim = line.split(':')
+#         runners[ext.strip()] = {'compile': comp.strip().replace(
+#             'NAME', Name), 'run': run.strip().replace('NAME', Name),'vim' : vim.strip()}
+#     line = runnersFile.readline()
+runners = loads(open('run.json', 'r').read())
 # print(runners)
 
 # getting the names of all template files with a corresponding extension in runners.txt
@@ -70,8 +71,19 @@ code['filecontents'] = open(templatenames[choice], 'r').read()
 
 # back to the launched directory
 chdir(wd)
-mkdir(Name) # making new directory for the problem
-chdir(Name) # changing to that directory
+mkdir(Name)  # making new directory for the problem
+chdir(Name)  # changing to that directory
+
+###############################################################################
+# making .conf file for the dog runner remove if unnecessary
+conf = open(".conf", 'w')
+conf.write(code['filename'] + '\n' +
+           'stdin.txt\n'
+           + runners[code['extension']
+                     ]['dogcommand'].replace('NAME', Name)+'\n'
+           )
+conf.close()
+###############################################################################
 
 # making the problem's code file and copying the template to it
 codeFile = open(code['filename'], 'w')
@@ -84,36 +96,43 @@ tcases = [str(i).replace('<pre>', '').replace(
 
 ##################################################################
 # This section is gvim specific. Comment it out for other editors.
-rcfile = open('rcfile','w')
-for i in range(1,5):
-    rcfile.write('silent! unmap <F%d>\nmap <F%d> :w <CR> :!'%(i,i)+runners[code['extension']]['vim']+' < stdin%d.txt <CR>\n'%(i))
+rcfile = open('rcfile', 'w')
+for i in range(1, 5):
+    rcfile.write('silent! unmap <F%d>\nmap <F%d> :w <CR> :!' % (
+        i, i)+runners[code['extension']]['run']+' < stdin%d.txt <CR>\n' % (i))
+rcfile.write('silent! unmap <F5>\nmap <F5> :w <CR> '+' && '.join(
+    [runners[code['extension']]['compile'], runners[code['extension']]['run']])+' <CR>')
 rcfile.close()
 ##################################################################
 
-runScript = open('run.sh','w')
+runScript = open('run.sh', 'w')
 runScript.write("set -e\n")
-runScript.write(runners[code['extension']]['compile']+"\n")
+runScript.write(runners[code['extension']]['compile'].replace(
+    '%<', Name).replace('%', Name+code['extension'])+"\n")
 runScript.write("set +e\n")
 
 
 c = 1
-for i in range(0,len(tcases),2):
-    f1 = open("stdin%d.txt"%(c),'w')
+for i in range(0, len(tcases), 2):
+    f1 = open("stdin%d.txt" % (c), 'w')
     f1.write('\n'.join(tcases[i]))
     f1.close()
-    f2 = open("stdout%d.txt"%(c),'w')
+    f2 = open("stdout%d.txt" % (c), 'w')
     f2.write('\n'.join(tcases[i+1])+'\n')
     f2.close()
-    runScript.write("echo \"TEST %d\" | tee%stimings.txt\n"%(c,' -a ' if c!=1 else ' '))
-    runScript.write("{ time timeout 5 %s < stdin%d.txt > out%d.txt ; } 2>&1 | sed -n 2p | tee -a timings.txt\n"%(runners[code['extension']]['run'],c,c))
-    runScript.write("comm stdout%d.txt out%d.txt\n"%(c,c))
-    c+=1
+    runScript.write("echo \"TEST %d\" | tee%stimings.txt\n" %
+                    (c, ' -a ' if c != 1 else ' '))
+    runScript.write("%s < stdin%d.txt > out%d.txt\n" %
+                    (runners[code['extension']]['scriptline'].replace('NAME', Name), c, c))
+    runScript.write("comm stdout%d.txt out%d.txt\n" % (c, c))
+    c += 1
 runScript.close()
 
 # calling gvim to open the directory with the files also opened
 # change this to suit your texteditor or ide
-system('gvim %s'%(code['filename']))
+# system('gvim %s'%(code['filename']))
 
-# system('code . '+code['filename'])
+system('code . '+code['filename'])
 
-# TODO: call the dog here
+# Calling the dog
+system('dog')
